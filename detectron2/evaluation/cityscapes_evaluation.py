0302d04dfce2842cb4fde173be197466f34edf9a
khsafkatamin <safkat.amin@rwth-aachen.de>
2025-06-10 13:02:19 +0200

    Refactor imports in cityscapes.py and cityscapes_evaluation.py to use updated cityscapesScripts path

--- detectron2/evaluation/cityscapes_evaluation.py
+++ detectron2/evaluation/cityscapes_evaluation.py
@@ -58,7 +58,8 @@ class CityscapesInstanceEvaluator(CityscapesEvaluator):
     """
 
     def process(self, inputs, outputs):
-        from deeplearning.projects.cityscapesApi.cityscapesscripts.helpers.labels import name2label
+        # from deeplearning.projects.cityscapesApi.cityscapesscripts.helpers.labels import name2label
+        from cityscapesScripts.cityscapesscripts.helpers.labels import name2label
 
         for input, output in zip(inputs, outputs):
             file_name = input["file_name"]
@@ -96,8 +97,9 @@ class CityscapesInstanceEvaluator(CityscapesEvaluator):
         comm.synchronize()
         if comm.get_rank() > 0:
             return
-        import deeplearning.projects.cityscapesApi.cityscapesscripts.evaluation.evalInstanceLevelSemanticLabeling as cityscapes_eval  # noqa: E501
-
+        # import deeplearning.projects.cityscapesApi.cityscapesscripts.evaluation.evalInstanceLevelSemanticLabeling as cityscapes_eval  # noqa: E501
+        import cityscapesScripts.cityscapesscripts.evaluation.evalInstanceLevelSemanticLabeling as cityscapes_eval
+        
         self._logger.info("Evaluating results under {} ...".format(self._temp_dir))
 
         # set some global states in cityscapes evaluation API, before evaluating
@@ -140,9 +142,10 @@ class CityscapesSemSegEvaluator(CityscapesEvaluator):
     """
 
     def process(self, inputs, outputs):
-        from deeplearning.projects.cityscapesApi.cityscapesscripts.helpers.labels import (
-            trainId2label,
-        )
+        # from deeplearning.projects.cityscapesApi.cityscapesscripts.helpers.labels import (
+        #     trainId2label,
+        # )
+        from cityscapesScripts.cityscapesscripts.helpers.labels import trainId2label
 
         for input, output in zip(inputs, outputs):
             file_name = input["file_name"]
@@ -163,7 +166,8 @@ class CityscapesSemSegEvaluator(CityscapesEvaluator):
             return
         # Load the Cityscapes eval script *after* setting the required env var,
         # since the script reads CITYSCAPES_DATASET into global variables at load time.
-        import deeplearning.projects.cityscapesApi.cityscapesscripts.evaluation.evalPixelLevelSemanticLabeling as cityscapes_eval  # noqa: E501
+        # import deeplearning.projects.cityscapesApi.cityscapesscripts.evaluation.evalPixelLevelSemanticLabeling as cityscapes_eval  # noqa: E501
+        import cityscapesScripts.cityscapesscripts.evaluation.evalPixelLevelSemanticLabeling as cityscapes_eval
 
         self._logger.info("Evaluating results under {} ...".format(self._temp_dir))
 
